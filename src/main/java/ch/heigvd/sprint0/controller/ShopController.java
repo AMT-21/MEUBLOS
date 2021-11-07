866b924e0e458e214c850e9dc5195af86c30ee4a
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2021-11-07 21:18:22 +0100

    Adding index template and a dummy test page navigation

--- src/main/java/ch/heigvd/sprint0/controller/ShopController.java
+++ src/main/java/ch/heigvd/sprint0/controller/ShopController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class ShopController {
 
-    @GetMapping("/")
+    @GetMapping("/shop")
     public String index() {
-        return "index.html";
+        return "shop.html";
     }
 }
