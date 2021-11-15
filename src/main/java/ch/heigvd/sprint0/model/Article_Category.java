b30f4f448c4e7881d1d658ae04dce83dce8b3c6c
danydacosta <danydc@powerfoxsa.ch>
2021-11-15 10:37:21 +0100

    override hashCode and equals

--- src/main/java/ch/heigvd/sprint0/model/Article_Category.java
+++ src/main/java/ch/heigvd/sprint0/model/Article_Category.java
@@ -2,6 +2,7 @@ package ch.heigvd.sprint0.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Meuble_Categorie")
@@ -15,4 +16,17 @@ public class Article_Category implements Serializable {
     @ManyToOne
     @JoinColumn(name = "nomCategorie", referencedColumnName = "nomCategorie")
     private Category category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article_Category that = (Article_Category) o;
+        return Objects.equals(article, that.article) && Objects.equals(category, that.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(article, category);
+    }
 }
--- src/main/java/ch/heigvd/sprint0/model/Cart_Article.java
+++ src/main/java/ch/heigvd/sprint0/model/Cart_Article.java
@@ -2,6 +2,7 @@ package ch.heigvd.sprint0.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Panier_Meuble")
@@ -18,4 +19,17 @@ public class Cart_Article implements Serializable {
 
     @Column(name = "quantite")
     private int quantity;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cart_Article that = (Cart_Article) o;
+        return quantity == that.quantity && Objects.equals(article, that.article) && Objects.equals(cart, that.cart);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(article, cart, quantity);
+    }
 }
