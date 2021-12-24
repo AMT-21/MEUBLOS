6e2250d3a94394685292a481e526e8cbfb356555
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2021-12-25 00:47:18 +0100

    fix test config and added shop page test.

--- src/test/java/ch/heigvd/sprint0/Sprint0ApplicationTests.java
+++ src/test/java/ch/heigvd/sprint0/Sprint0ApplicationTests.java
@@ -1,6 +1,10 @@
 package ch.heigvd.sprint0;
 
+import org.flywaydb.core.Flyway;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,18 +21,65 @@ import org.springframework.test.web.servlet.MockMvc;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
 class Sprint0ApplicationTests {
 
     @Autowired
     private MockMvc mvc;
 
+    @Autowired
+    private Flyway flyway;
+
+    @BeforeAll
+    public void init() { // Reset database content
+        flyway.clean();
+        flyway.migrate();
+    }
+
     @Test
-    void shop() throws Exception {
+    void displayArticlesInIndex() throws Exception {
         mvc.perform(get("/"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Table en marbre")));
+                .andExpectAll(
+                        content().string(containsString("Un meuble du grenier")),
+                        content().string(containsString("Table en marbre")),
+                        content().string(containsString("Armoire style &quot;Louis XVI&quot;")));
+    }
+
+    @Test
+    void displayAllArticlesInShop() throws Exception {
+        mvc.perform(get("/shop"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpectAll(
+                        content().string(containsString("Un meuble du grenier")),
+                        content().string(containsString("Table en marbre")),
+                        content().string(containsString("Armoire style &quot;Louis XVI&quot;")));
+    }
+
+
+    @Test
+    void displayFilteredArticlesInShop() throws Exception {
+        mvc.perform(get("/shop?namecategory=Table"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpectAll(
+                        content().string(CoreMatchers.not(containsString("Un meuble du grenier"))),
+                        content().string(containsString("Table en marbre")),
+                        content().string(CoreMatchers.not(containsString("Armoire style &quot;Louis XVI&quot;"))));
+    }
+
+    @Test
+    void displayDetailArticle() throws Exception {
+        mvc.perform(get("/shop/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpectAll(
+                        content().string(containsString("Un meuble du grenier")),
+                        content().string(containsString("Il était dans mon grenier pendant des années")),
+                        content().string(containsString("Ajouter au panier")));
     }
 
 }
