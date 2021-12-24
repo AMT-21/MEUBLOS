b8c4cdda07d73103b37104d34a0ebb02e093b1b0
JunodChristophe <49147076+JunodChristophe@users.noreply.github.com>
2021-12-24 18:26:47 +0100

    First unit test, test

--- src/main/java/ch/heigvd/sprint0/controller/IndexController.java
+++ src/main/java/ch/heigvd/sprint0/controller/IndexController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -23,7 +25,8 @@ public class IndexController {
     }
 
     @GetMapping("/")
-    public String index(Model model, HttpSession session) {
+    public String index(Model model, HttpSession session
+            , HttpServletRequest req, HttpServletResponse resp) {
         model.addAttribute("articles", utils.getArticlesInfo(articleService.find3LatestArticles(), session));
         return "index";
     }
--- src/test/java/ch/heigvd/sprint0/Sprint0ApplicationTests.java
+++ src/test/java/ch/heigvd/sprint0/Sprint0ApplicationTests.java
@@ -1,14 +1,35 @@
 package ch.heigvd.sprint0;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application.properties")
 class Sprint0ApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    void shop() throws Exception {
+        mvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Table en marbre")));
     }
 
 }
